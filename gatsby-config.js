c0ee1d161dad609f6f4d4e7cdfadffa7d4c0b7d0
ekaone <ekaone3033@gmail.com>
2020-05-27 13:20:46 +0700

    change meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,58 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+  path: `.env`
+});
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `GatsbyJS Indonesia`,
+    siteTitleAlt: `Komunitas GatsbyJS Indonesia`,
+    siteHeadline: `GatsbyJS Indonesia`,
+    siteUrl: `https://gatsbyjs.id/`,
+    siteDescription: `Komunitas GatsbyJS Indonesia, pembawa perubahan besar untuk Bangsa dan Negara`,
+    siteLanguage: `id`,
+    siteImage: `/banner.jpg`,
+    author: `@dannyeka`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        short_name: `GatsbyJS-ID`,
+        description: `Komunitas GatsbyJS Indonesia, pembawa perubahan besar untuk Bangsa dan Negara`,
         start_url: `/`,
         background_color: `#141821`,
-        theme_color: `#f6ad55`,
+        theme_color: `#663399`,
         display: `standalone`,
         icons: [
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-netlify`
+  ]
+};
